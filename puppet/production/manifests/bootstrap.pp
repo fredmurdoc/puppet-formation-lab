5bb16a4d33f2e2fde612a5bddebca8fd51e50314
fgontier <fgontier@stagiaire.org>
2019-10-16 12:35:53 +0200

    ajout de resources dans HIERA utilisation de la convention class::variable pour alimenter directmeent la classe bootstrap

--- puppet/production/manifests/bootstrap.pp
+++ puppet/production/manifests/bootstrap.pp
@@ -2,10 +2,18 @@
 #
 #
 
-class bootstrap {
+class bootstrap(
+  $users,
+  $groups,
+  $files,
+  $ssh_authorized_keys
+  ) {
   # declaration de la classes avec include
   include vars
   class {'manage_ssh':}
-  create_resources('user', hiera('bootstrap::users'))
+  create_resources('user', $users)
+  create_resources('group', $groups)
+  create_resources('file', $files)
+  create_resources('ssh_authorized_key', $ssh_authorized_keys)
 
 }
